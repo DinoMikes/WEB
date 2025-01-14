f590799d07688737eb07d262141fbb7b462ebf6d
DinoMikes <Reereer02@yandex.ru>
2025-01-14 10:08:26 +0500

    adding files

---
+++ lab7/index.js
@@ -0,0 +1,6 @@
+var video = document.createElement("video");
+    video.src = "video.mp4";
+    video.width = 640;
+    video.height = 360;
+    video.controls = true;
+    document.getElementById("videoContainer").appendChild(video);
